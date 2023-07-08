576b9e4a3fac465ede36030c9300bf1664ec0fd9
ephilipp <eduardo.philipp@fcecon.unr.edu.ar>
2023-07-07 22:20:11 -0300

    dw

--- src/workflow-inicial/wfv8/wfv8641_TS_training_strategy.r
+++ src/workflow-inicial/wfv8/wfv8641_TS_training_strategy.r
@@ -11,18 +11,15 @@ require("yaml")
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "WFV5TS6410"
+PARAM$experimento <- "WFV8TS6410"
 
-PARAM$exp_input <- "WFV5FE6310"
+PARAM$exp_input <- "WFV8FE6310"
 
 # me salteo los meses duros de pandemia, pero llego hasta 201907 en training
 # entreno en 18 meses
 
-PARAM$future <- c(202002)
+PARAM$future <- c(201911)
 PARAM$final_train <- c(
-  201912, 
-  201911, 
-  201910,
   201909,
   201908,
   201907,
@@ -33,9 +30,6 @@ PARAM$final_train <- c(
 )
 
 PARAM$train$training <- c(
-  201910,
-  201909,
-  201908,
   201907,
   201906,
   201905,
@@ -45,8 +39,8 @@ PARAM$train$training <- c(
   201901
 )
 
-PARAM$train$validation <- c(202011)
-PARAM$train$testing <- c(202012)
+PARAM$train$validation <- c(202008)
+PARAM$train$testing <- c(202009)
 
 # Atencion  0.1  de  undersampling de la clase mayoritaria,  los CONTINUA
 # 1.0 significa NO undersampling ,  0.1  es quedarse con el 10% de los CONTINUA
