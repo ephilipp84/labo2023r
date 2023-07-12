a9332a4389e9aafd73c3dcde5caab32409c46d6c
ephilipp <eduardo.philipp@fcecon.unr.edu.ar>
2023-07-12 06:32:01 -0300

    432

--- src/workflow-inicial/wfv3hisanid/wfv3his601_RUN_correr_workflow.r
+++ src/workflow-inicial/wfv3hisanid/wfv3his601_RUN_correr_workflow.r
@@ -6,18 +6,6 @@ options(error = function() {
   stop("exiting after script error")
 })
 
-#wfv2anid
-# primeros pasos, relativamente rapidos
-source("~/labo2023r/src/workflow-inicial/wfv2anid/wfv2611_CA_reparar_dataset.r")
-source("~/labo2023r/src/workflow-inicial/wfv2anid/wfv2621_DR_corregir_drifting.r")
-source("~/labo2023r/src/workflow-inicial/wfv2anid/wfv2631_FE_historia.r")
-source("~/labo2023r/src/workflow-inicial/wfv2anid/wfv2641_TS_training_strategy.r")
-
-# ultimos pasos, muy lentos
-source("~/labo2023r/src/workflow-inicial/wfv2anid/wfv2651_HT_lightgbm.r")
-source("~/labo2023r/src/workflow-inicial/wfv2anid/wfv2661_ZZ_final.r")
-
-
 
 #wfv3hisanid
 # corrida de cada paso del workflow
@@ -31,3 +19,6 @@ source("~/labo2023r/src/workflow-inicial/wfv3hisanid/wfv3his641_TS_training_stra
 # ultimos pasos, muy lentos
 source("~/labo2023r/src/workflow-inicial/wfv3hisanid/wfv3his651_HT_lightgbm.r")
 source("~/labo2023r/src/workflow-inicial/wfv3hisanid/wfv3his661_ZZ_final.r")
+
+# corrida de cada paso del workflow
+
