20645c34f4533a3bce8700fa7fa19ebf1428cfbd
ephilipp84 <eduardo.philipp@fcecon.unr.edu.ar>
2023-07-16 20:57:59 +0000

    d

--- src/workflow-semillerio/kaggle1/701_RUN_correr_workflow.r
+++ src/workflow-semillerio/kaggle1/701_RUN_correr_workflow.r
@@ -17,4 +17,4 @@ source("~/labo2023r/src/workflow-semillerio/kaggle1/751_HT_lightgbm.r")
 
 
 source("~/labo2023r/src/workflow-semillerio/kaggle1/771_ZZ_final_semillerio.r")
-source("~/labo2023r/src/workflow-semillerio/kaggle1/781_ZZ_semillerios_hibridacion.r")
+#source("~/labo2023r/src/workflow-semillerio/kaggle1/781_ZZ_semillerios_hibridacion.r")
