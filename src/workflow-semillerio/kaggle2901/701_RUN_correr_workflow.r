67361d0aa92df88d999c4e988dcf067e9494aca3
ephilipp <eduardo.philipp@fcecon.unr.edu.ar>
2023-07-18 10:14:40 -0300

    fe

---
+++ src/workflow-semillerio/kaggle2901/701_RUN_correr_workflow.r
@@ -0,0 +1,19 @@
+# Corrida general del workflow Semillerio
+
+options(error = function() {
+  traceback(20)
+  options(error = NULL)
+  stop("exiting after script error")
+})
+
+# corrida de cada paso del workflow
+source("~/labo2023r/src/workflow-semillerio/kaggle2901/711_CA_reparar_dataset.r")
+source("~/labo2023r/src/workflow-semillerio/kaggle2901/721_DR_corregir_drifting.r")
+source("~/labo2023r/src/workflow-semillerio/kaggle2901/731_FE_historia.r")
+source("~/labo2023r/src/workflow-semillerio/kaggle2901/741_TS_training_strategy.r")
+source("~/labo2023r/src/workflow-semillerio/kaggle2901/751_HT_lightgbm.r")
+
+
+
+source("~/labo2023r/src/workflow-semillerio/kaggle2901/771_ZZ_final_semillerio.r")
+#source("~/labo2023r/src/workflow-semillerio/kaggle2/781_ZZ_semillerios_hibridacion.r")
